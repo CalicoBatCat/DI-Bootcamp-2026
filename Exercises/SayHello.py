5a807e0e221eeee41d17c17a4650c1a10c7b919c
CalicoBatCat <bekguida@gmail.com>
2026-07-29 14:27:08 +0300

    Delete Exercises directory

--- Exercises/SayHello.py
+++
@@ -1,5 +0,0 @@
-#Map, Filter. Say Hello to <= 4 letter names in string
-people = ["Rick", "Morty", "Beth", "Jerry", "Snowball"]
-map_object = map(lambda name: f"Hello, {name}.", filter(lambda name: len(name) <= 4, people))
-
-print(list(map_object))
